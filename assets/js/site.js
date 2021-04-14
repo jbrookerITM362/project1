886ed664af7af7cb9fa2429d5e95958848f51f07
Jacob Brooker <70109486+jab12444@users.noreply.github.com>
2021-04-13 19:46:24 -0500

    Remove unnecessary code

--- assets/js/site.js
+++ assets/js/site.js
@@ -4,12 +4,6 @@ jQuery(function($) {
   $('html').addClass('hasjs');
 });
 
-function myFunction() {
-  var x = document.getElementById("intro");
-  x.style.fontSize = "16px"; 
-  x.style.color = "#334d4d"; 
-}
-
 /*button submit */
 function mySubmission() {
   alert('Thank you, our newsletter will be sent to your email shortly. Save the whales!');
